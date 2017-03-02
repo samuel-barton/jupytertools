cc9a24e512f8ed899f301c4da57601c34ca71038
Samuel Barton <samuel.barton@maine.edu>
2017-03-02 13:46:14 -0500

    Added setup file to enable installation of plotting package with pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='JupyterTools',
+      version='1.0',
+      description='Tools for Scientific Programming with Jupyter Notebooks',
+      author='Samuel Barton',
+      author_email='samwisebarton@gmail.com',
+      url='https://github.com/samuel-barton/jupytertools',
+      py_modules['plotting']
+     )
