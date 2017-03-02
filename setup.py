bcfe9d68040ec725d86032e846cb541329ab48b5
Samuel Barton <samuel.barton@maine.edu>
2017-03-02 13:49:44 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='plotting',
       author='Samuel Barton',
       author_email='samwisebarton@gmail.com',
       url='https://github.com/samuel-barton/jupytertools',
-      py_modules['plotting'],
+      py_modules=['plotting'],
      )
