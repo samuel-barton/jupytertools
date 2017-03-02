efc595d31f695fcde03f5daad7a4d6c59e4041fc
Samuel Barton <samuel.barton@maine.edu>
2017-03-02 13:48:18 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 
-setup(name='JupyterTools',
+setup(name='plotting',
       version='1.0',
       description='Tools for Scientific Programming with Jupyter Notebooks',
       author='Samuel Barton',
