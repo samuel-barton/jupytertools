dcac8879986cbe983944968d90565bd75415e638
Samuel Barton <samuel.barton@maine.edu>
2017-03-02 14:26:39 -0500

    added dependency checking to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name='jupytertools',
       author_email='samwisebarton@gmail.com',
       url='https://github.com/samuel-barton/jupytertools',
       packages=['jupytertools'],
+      install_requires=['matplotlib'],
      )
