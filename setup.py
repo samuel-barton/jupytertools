eb58464987adb4dd087fa4e2fbb84e46cb6b76e6
Samuel Barton <samuel.barton@maine.edu>
2017-03-21 13:25:39 -0400

    Fixed issues with __init__.py files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='jupytertools',
-      version='1.12',
+      version='1.13',
       description='Tools for Scientific Programming with Jupyter Notebooks',
       author='Samuel Barton',
       author_email='samwisebarton@gmail.com',
