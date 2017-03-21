4a89947dcc2bf67036650d5a97ca23f60b596b0f
Samuel Barton <samuel.barton@maine.edu>
2017-03-21 12:32:52 -0400

    Changed version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='jupytertools',
-      version='1.0',
+      version='1.1',
       description='Tools for Scientific Programming with Jupyter Notebooks',
       author='Samuel Barton',
       author_email='samwisebarton@gmail.com',
